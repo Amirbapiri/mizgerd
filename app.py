dbb0eba5d5d6d34fd274b8cfb83335263339b91f
amir <amirbapiri@gmail.com>
2020-12-25 23:24:51 +0330

    retrieve and insert event

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 
 app = Flask(__name__)
 api = Api(app)
 
+events = []
+
 
 class Event(Resource):
-    def get(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument("title", required=True,
+                        help="This filed can not be empty.")
+    parser.add_argument("description", required=False)
+    parser.add_argument("dates", required=True)
+
+    def get(self, title):
         """
-        Retrieve all events
+        Retrieve a specific event based on title
         """
-        return {"Event": "something new "}
+        event = next(filter(lambda e: e["title"] == title, events), None)
+        return {"event": event}, 200 if event else 404
 
     def post(self):
         """
         Insert new event
         """
-        pass
+        data = Event.parser.parse_args()
+        event = {
+            "title": data["title"],
+            "description": data["description"],
+            "dates": data["dates"],
+        }
+        events.append(event)
+        return event, 201
 
 
-api.add_resource(Event, "/event")
+class EventList(Resource):
+    def get(self):
+        return events
+
+
+api.add_resource(Event, "/event", "/event/<string:title>")
+api.add_resource(EventList, "/event/list")
 
 if __name__ == "__main__":
     app.run()
