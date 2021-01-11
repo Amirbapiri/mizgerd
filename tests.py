7f9c24bc20752e402b2d96527e01769164072269
amir <amirbapiri@gmail.com>
2021-01-11 12:36:14 +0330

    Created test_create

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+import json
+
+from app import app
+
+
+class TestEvents(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_create(self):
+        payload = {
+            "title": "TestTitle",
+            "description": "TestDescription",
+            "dates": [{"year": 2020, "month": 2, "day": 4, "times": {}}],
+        }
+        response = self.app.post(
+            "/event",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(payload),
+        )
+        self.assertDictEqual(payload, response.json)
+        self.assertEqual(201, response.status_code)
